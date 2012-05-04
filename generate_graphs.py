9d0324c8988a0b4b37c027cad7590ffa94f8c0fd
Thomas Steinbrenner <thomas@thomas-steinbrenner.net>
2012-05-05 00:20:12 +0200

    worked on the graphs

--- generate_graphs.py
+++ generate_graphs.py
@@ -24,8 +24,9 @@ def main():
     print str(db_cursor.rowcount) + " processed apps found in the database"
     apps_processed = db_cursor.fetchall()
 
+    ###
     # apps_per_category
-    db_cursor.execute('SELECT category, COUNT(name) from applications GROUP BY category ORDER BY count DESC')
+    db_cursor.execute('SELECT (SELECT name FROM categories WHERE id = category) AS category, COUNT(name) from applications GROUP BY category ORDER BY count DESC')
     app_count = db_cursor.fetchall()
 
     app_count_names = []
@@ -35,8 +36,48 @@ def main():
         app_count_names.append(str(group[0]))
         app_count_data.append(int(group[1]))
 
-    cairoplot.vertical_bar_plot ( path + 'apps_per_category.svg', app_count_data, 1200, 300, border = 20, display_values = True, grid = False, x_labels = app_count_names)
+    cairoplot.vertical_bar_plot ( path + 'apps_per_category.svg', app_count_data, 800, 700, border = 20, display_values = True, grid = False, series_labels = app_count_names) 
 
+
+    ###
+    # top 25 permissions (TODO: percent)
+    db_cursor.execute('SELECT (SELECT regex from permissions WHERE id = permission_id), COUNT(*) from applications_permissions GROUP BY permission_id ORDER BY count DESC LIMIT 25')
+    top25permissions_count = db_cursor.fetchall()
+
+    top25permissions_count_names = []
+    top25permissions_count_data = []
+
+    for group in top25permissions_count:
+        top25permissions_count_names.append(str(group[0]))
+        top25permissions_count_data.append(int(group[1]))
+
+    cairoplot.vertical_bar_plot ( path + 'top25permissions.svg', top25permissions_count_data, 800, 500, border = 5, display_values = True, grid = False, series_labels = top25permissions_count_names)
+
+
+    ###
+    # top 25 developers
+    db_cursor.execute('SELECT developer, COUNT(developer) from applications GROUP BY developer ORDER BY count DESC LIMIT 25')
+    top25developers_count = db_cursor.fetchall()
+
+    top25developers_count_names = []
+    top25developers_count_data = []
+
+    for group in top25developers_count:
+        top25developers_count_names.append(str(group[0]))
+        top25developers_count_data.append(int(group[1]))
+
+    cairoplot.vertical_bar_plot ( path + 'top25developers.svg', top25developers_count_data, 800, 700, border = 5, display_values = True, grid = False, series_labels = top25developers_count_names)
+
+    ###
+    # SELECT (SELECT COUNT(name) FROM applications WHERE price = '0') AS "Free", (SELECT COUNT(name) FROM applications WHERE price != '0') AS "Paid"
+
+    ###
+    # permissions per category
+    db_cursor.execute('SELECT (SELECT regex from permissions WHERE id = permission_id) AS permission, COUNT(*) from applications_permissions GROUP BY permission_id ORDER BY count DESC')
+
+
+
+    
 
 
     ####
@@ -49,7 +90,7 @@ def main():
     h = [10*math.sin(x) for x in t]
     erx = [0.1*random.random() for x in t]
     ery = [5*random.random() for x in t]
-    data = {"exp" : [t,f], "cos" : [t,g], "sin" : [t,h]}
+    data = {"exp" : [t,f], "cos" : [t,g]}
     series_colors = [ (1,0,0), (0,0,0), (0,0,1) ]
 
     cairoplot.scatter_plot ( path + 'test.svg', data = data, errory = [ery,ery], width = 800, height = 600, border = 20, 
