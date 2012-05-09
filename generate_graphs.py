1a547843b72669b903001aaa11de0fbb7ac1d876
Thomas Steinbrenner <thomas@thomas-steinbrenner.net>
2012-05-09 15:25:22 +0200

    first version with mathplotlib

--- generate_graphs.py
+++ generate_graphs.py
@@ -7,6 +7,11 @@ import random
 
 import cairoplot
 
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pyplot
+
 from config import *
 
 def main():
@@ -33,26 +38,41 @@ def main():
     app_count_data = []
 
     for group in app_data:
-        app_count_names.append(str(group[0]))
-        app_count_data.append(int(group[1]))
+        app_count_names.insert(0, str(group[0]).lower().replace('_', ' '))
+        app_count_data.insert(0, int(group[1]))
 
-    cairoplot.vertical_bar_plot ( path + 'apps_per_category.svg', app_count_data, 800, 700, border = 20, display_values = True, grid = False, series_labels = app_count_names) 
-
+    fig = pyplot.figure()
+    fig.subplots_adjust(left=0.3)
+    pyplot.barh(bottom=drange(0.5, len(app_count_names)+0.5, 1), width=app_count_data)
+    pyplot.title('Applications per category')
+    pyplot.xlabel('number of applications')
+    pos = np.arange(len(app_count_names))+0.9
+    pyplot.yticks(pos, app_count_names)
+    pyplot.grid(True)
+    pyplot.savefig( path + 'apps_per_category.svg')
 
     ###
     # top 25 permissions (TODO: percent)
-    db_cursor.execute('SELECT (SELECT regex from permissions WHERE id = permission_id), COUNT(*) from applications_permissions GROUP BY permission_id ORDER BY count DESC LIMIT 25')
+    db_cursor.execute('SELECT (SELECT regex from permissions WHERE id = permission_id) AS permission, COUNT(*)::float/(SELECT COUNT(name)::float FROM applications) AS percent FROM applications_permissions GROUP BY permission_id ORDER BY percent DESC LIMIT 25')
     top25permissions_data = db_cursor.fetchall()
 
     top25permissions_count_names = []
     top25permissions_count_data = []
 
     for group in top25permissions_data:
-        top25permissions_count_names.append(str(group[0]))
-        top25permissions_count_data.append(int(group[1]))
+        top25permissions_count_names.insert(0, str(group[0]))
+        top25permissions_count_data.insert(0, float(group[1]))
 
-    cairoplot.vertical_bar_plot ( path + 'top25permissions.svg', top25permissions_count_data, 800, 500, border = 5, display_values = True, grid = False, series_labels = top25permissions_count_names)
-
+    fig = pyplot.figure(figsize=(12, 6))
+    fig.subplots_adjust(left=0.5)
+    pyplot.barh(bottom=drange(0.5, len(top25permissions_count_names)+0.5, 1), width=top25permissions_count_data)
+    pyplot.title('Top 25 requested permissions')
+    pyplot.xlabel('percentage of applications')
+    pos = np.arange(len(top25permissions_count_names))+0.9
+    pyplot.yticks(pos, top25permissions_count_names)
+    pyplot.grid(True)
+    pyplot.savefig( path + 'top25permissions.svg')
+    
 
     ###
     # top 25 developers
@@ -63,10 +83,18 @@ def main():
     top25developers_count_data = []
 
     for group in top25developers_data:
-        top25developers_count_names.append(str(group[0]))
-        top25developers_count_data.append(int(group[1]))
+        top25developers_count_names.insert(0, str(group[0]))
+        top25developers_count_data.insert(0, int(group[1]))
 
-    cairoplot.vertical_bar_plot ( path + 'top25developers.svg', top25developers_count_data, 800, 700, border = 5, display_values = True, grid = False, series_labels = top25developers_count_names)
+    fig = pyplot.figure()
+    fig.subplots_adjust(left=0.4)
+    pyplot.barh(bottom=drange(0.5, len(top25developers_count_names)+0.5, 1), width=top25developers_count_data)
+    pyplot.title('Top 25 developers')
+    pyplot.xlabel('number of applications')
+    pos = np.arange(len(top25developers_count_names))+0.9
+    pyplot.yticks(pos, top25developers_count_names)
+    pyplot.grid(True)
+    pyplot.savefig( path + 'top25developers.svg')
 
 
     ###
@@ -76,47 +104,50 @@ def main():
     ###
     # Free vs Paid
 
-#SELECT p.id
-#     ,(100 * sum((a.price > 0)::int)) / cc.ct AS commercial
-#     ,(100 * sum((a.price = 0)::int)) / cf.ct AS free
-#FROM  (SELECT count(*)::float AS ct FROM applications WHERE price > 0) AS cc
-#      ,(SELECT count(*)::float AS ct FROM applications WHERE price = 0) AS cf
-#      ,permissions p
-#LEFT   JOIN applications_permissions ap ON ap.permission_id = p.id
-#LEFT   JOIN applications a ON a.id = ap.application_id
-#WHERE p.regex is not Null
-#GROUP  BY 1, cc.ct, cf.ct
-#ORDER  BY 2 DESC, 3 DESC, 1;
+#    db_cursor.execute('SELECT p.id \
+    db_cursor.execute('SELECT (SELECT regex FROM permissions WHERE id = p.id) AS regex \
+                       ,(sum((a.price > 0)::int)) / cc.ct AS commercial \
+                       ,(sum((a.price = 0)::int)) / cf.ct AS free \
+        FROM  (SELECT count(*)::float AS ct FROM applications WHERE price > 0) AS cc \
+             ,(SELECT count(*)::float AS ct FROM applications WHERE price = 0) AS cf \
+             , permissions p \
+        LEFT JOIN applications_permissions ap ON ap.permission_id = p.id \
+        LEFT JOIN applications a ON a.id = ap.application_id \
+        WHERE p.regex is not Null \
+        GROUP BY 1, cc.ct, cf.ct \
+        ORDER BY 2 DESC, 3 DESC, 1')
+    permissions_data = db_cursor.fetchall()
 
-    db_cursor.execute('SELECT (SELECT regex FROM permissions WHERE id = applications_permissions.permission_id) AS "regex", 100::float * COUNT(*)/(SELECT COUNT(name) FROM applications WHERE price = \'0\') AS "percent" FROM applications, applications_permissions WHERE applications.id = applications_permissions.application_id AND applications.price = \'0\' GROUP BY applications_permissions.permission_id ORDER BY percent DESC')
-    freepermissions_data = db_cursor.fetchall()
-    db_cursor.execute('SELECT (SELECT regex FROM permissions WHERE id = applications_permissions.permission_id) AS "regex", 100::float * COUNT(*)/(SELECT COUNT(name) FROM applications WHERE price != \'0\') AS "percent" FROM applications, applications_permissions WHERE applications.id = applications_permissions.application_id AND applications.price != \'0\' GROUP BY applications_permissions.permission_id ORDER BY percent DESC')
-    paidpermissions_data = db_cursor.fetchall()
+    permissions_names = []
+    permissions_commcount = []
+    permissions_freecount = []
+  
+    for permission in permissions_data:
+        permissions_names.insert(0, str(permission[0]))
+        permissions_commcount.insert(0, float(permission[1]))
+        permissions_freecount.insert(0, float(permission[2]))
 
-    freepermissions_names = []
-    freepermissions_count = []
-    paidpermissions_names = []
-    paidpermissions_count = []
+    fig = pyplot.figure(figsize=(36, 36))
+    fig.subplots_adjust(left=0.22)
+  
+    pos = np.arange(len(permissions_names))
+    height = 0.35
+    
+    rects1 = pyplot.barh(pos, width=permissions_commcount, height=0.35, color='r')
+    rects2 = pyplot.barh(pos-height, width=permissions_freecount, height=0.35, color='g')
+    
+    pyplot.legend( (rects1[0], rects2[0]), ('Commercial', 'Free') )
 
-    for permission in freepermissions_data:
-        freepermissions_names.append(str(permission[0]))
-        freepermissions_count.append(int(permission[1]))
-
-    for permisssion in paidpermissions_data:
-        paidpermissions_names.append(str(permission[0]))
-        paidpermissions_count.append(int(permission[1]))
-
-    cairoplot.vertical_bar_plot ( path + 'freevspaid.svg', data = {'Free': freepermissions_count, 'Paid': paidpermissions_count},
-                                  width = 8000, height = 600, border = 5,
-                                  display_values = True,
-                                  grid = True)
+    pyplot.title('Commercial vs free applications')
+    pyplot.xlabel('percentage of applications')
+    pyplot.yticks(pos, permissions_names)
+    pyplot.grid(True)
+    pyplot.savefig( path + 'freevscommpermissions.svg')
 
 
     ###
     # permissions per category
     db_cursor.execute('SELECT (SELECT regex from permissions WHERE id = permission_id) AS permission, COUNT(*) from applications_permissions GROUP BY permission_id ORDER BY count DESC')
-
-
 
     ####
     categories = 'a b c d e f g h'.split()
@@ -135,6 +166,14 @@ def main():
                              axis = True, discrete = False, dots = 5, grid = True, 
                              x_title = "t", y_title = "f(t) g(t)", series_legend=True, series_colors = ["red", "blue", "orange"])
 
+
+def drange(start, stop, step):
+    list = []    
+    i = start
+    while i < stop:
+        list.append(i)
+        i += step
+    return list
 
 if __name__ == '__main__':
     main()
